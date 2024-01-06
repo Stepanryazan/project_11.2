a3e75b39c971ab0b151131b1ac5cc25cc5e3a96e
Stepanryazan <154748191+Stepanryazan@users.noreply.github.com>
2024-01-06 21:46:09 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,15 +1,5 @@
 def str_title(word):
-    """делает все буквы заглавными"""
     return word.upper()
 
 word = str(input())
 print(str_title(word))
-
-
-def word_title(word):
-    """делпет первые буквы заглавными"""
-    return word.title()
-
-
-word = str(input())
-print(word_title(word))
